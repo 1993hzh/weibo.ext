2afc8560c1e5d4da5dfb8bf29df502c2d4e1c6ed
Leo <mail@huzhonghua.cn>
2016-03-30 23:00:19 +0800

    optimize the logic for block

--- background.js
+++ background.js
@@ -14,11 +14,20 @@ Background.blockPerson = function (id, name, callback) {
     });
 };
 
-Background.getBlockedPerson = function (id, name, callback) {
+Background.getBlockedPerson = function (callback) {
     Util.storage.getValue("blockedPerson", function (obj) {
         var blockedPerson = Util.storage.isEmpty(obj) ? [] : obj["blockedPerson"];
         if (callback) {
             callback(blockedPerson);
+        }
+    });
+};
+
+Background.isBlockEnabled = function (callback) {
+    Util.storage.getValue("blockOption", function (obj) {
+        var value = !Util.storage.isEmpty(obj) ? obj["blockOption"] : true;
+        if (callback) {
+            callback(value !== "false");
         }
     });
 };
@@ -33,16 +42,34 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
             });
             return true;
         case "blockPerson":
-            Background.blockPerson(request.id, request.name, function (result) {
-                sendResponse({
-                    result: result
+            Background.isBlockEnabled(function(isBlockEnabled) {
+                if (!isBlockEnabled) {
+                    sendResponse({
+                        result: Util.opt(false, "没有启用屏蔽用户功能。")
+                    });
+                    return true;
+                }
+
+                Background.blockPerson(request.id, request.name, function(result) {
+                    sendResponse({
+                        result: result
+                    });
                 });
             });
             return true;
         case "getBlockedPerson":
-            Background.getBlockedPerson(request.id, request.name, function (result) {
-                sendResponse({
-                    result: Util.opt(true, result)
+            Background.isBlockEnabled(function(isBlockEnabled) {
+                if (!isBlockEnabled) {
+                    sendResponse({
+                        result: Util.opt(true, [])
+                    });
+                    return true;
+                }
+
+                Background.getBlockedPerson(function (result) {
+                    sendResponse({
+                        result: Util.opt(true, result)
+                    });
                 });
             });
             return true;
--- js/setup.js
+++ js/setup.js
@@ -4,9 +4,20 @@ Setup.showResult = function(result) {
     var success = document.getElementById("success");
     var fail = document.getElementById("fail");
 
+    var createCloseNode = function() {
+        var close = document.createElement("i");
+        close.setAttribute("class", "fa fa-times pull-right");
+        close.style.cursor = "pointer";
+        close.addEventListener("click", function(e) {
+            e.currentTarget.parentElement.style.display = "none";
+        }, false);;
+        return close;
+    };
+
     var show = function(node, msg) {
         node.style.display = "block";
-        node.innerHTML = msg;
+        node.textContent = msg;
+        node.appendChild(createCloseNode());
     };
 
     var hide = function(node) {
@@ -27,34 +38,35 @@ Setup.load = function() {
 
     // load messageOption
     Util.storage.getValue(options[0], function(obj) {
-        if (!Util.storage.isEmpty(obj)) {
-            document.querySelector("input[name=" + options[0] + "][value=" + obj[options[0]] + "]").setAttribute("checked", true);
-        }
+        var value = !Util.storage.isEmpty(obj) ? obj[options[0]] : true;
+        document.querySelector("input[name=" + options[0] + "][value=" + value +"]").setAttribute("checked", true);
     });
 
     // load blockOption
     Util.storage.getValue(options[1], function(obj) {
-        if (!Util.storage.isEmpty(obj)) {
-            document.querySelector("input[name=" + options[1] + "][value=" + obj[options[1]] + "]").setAttribute("checked", true);
-        }
+        var value = !Util.storage.isEmpty(obj) ? obj[options[1]] : true;
+        document.querySelector("input[name=" + options[1] + "][value=" + value + "]").setAttribute("checked", true);
+
+        // load blocked person
+        Util.storage.getValue(options[3], function(obj) {
+            if (!Util.storage.isEmpty(obj)) {
+                var person = obj[options[3]];
+                var targetNode = document.getElementById(options[3]);
+                if (value === "false") {
+                	targetNode.style.display = "none";
+                }
+                for (i in person) {
+                    var newNode = Setup.createBlockNodes(person[i].id, person[i].name);
+                    targetNode.appendChild(newNode);
+                }
+            }
+        });
     });
 
     // load diy css
     Util.storage.getValue(options[2], function(obj) {
         if (!Util.storage.isEmpty(obj)) {
             document.getElementById(options[2]).textContent = obj[options[2]];
-        }
-    });
-
-    // load blocked person
-    Util.storage.getValue(options[3], function(obj) {
-        if (!Util.storage.isEmpty(obj)) {
-            var person = obj[options[3]];
-            var targetNode = document.getElementById(options[3]);
-            for (i in person) {
-                var newNode = Setup.createBlockNodes(person[i].id, person[i].name);
-                targetNode.appendChild(newNode);
-            }
         }
     });
 };
@@ -71,6 +83,26 @@ Setup.registerRadioHandler = function() {
             var key = e.currentTarget.getAttribute("name");
             var value = e.currentTarget.getAttribute("value");
             Util.storage.setValue(key, value, Setup.showResult);
+        }, false);
+    });
+};
+
+Setup.registerBlockOptionHandler = function() {
+    var blockOptions = document.getElementsByName('blockOption');
+    if (!blockOptions) {
+        Setup.showResult(Util.opt(false, "Cannot find blockOption here."));
+        return;
+    }
+    var blockOptionArray = Util.nodeListToArray(blockOptions);
+    blockOptionArray.forEach(function(r) {
+        r.addEventListener("change", function(e) {
+            var value = e.currentTarget.getAttribute("value");
+            var div = document.getElementById("blockedPerson");
+            if (value === "false") {
+            	div.style.display = "none";
+            } else {
+            	div.style.display = "block";
+            }
         }, false);
     });
 };
@@ -96,11 +128,11 @@ Setup.registerDiyCssHandler = function() {
 };
 
 Setup.close = function(e) {
-	var targetNode = e.currentTarget.parentElement;
+    var targetNode = e.currentTarget.parentElement;
     var id = targetNode.getAttribute("id");
     if (!id) {
-    	Setup.showResult(Util.opt(false, "No target id found."));
-    	return false;
+        Setup.showResult(Util.opt(false, "No target id found."));
+        return false;
     }
     Util.storage.getValue("blockedPerson", function(obj) {
         if (!Util.storage.isEmpty(obj)) {
@@ -136,6 +168,7 @@ Setup.createBlockNodes = function(id, name) {
 document.addEventListener("DOMContentLoaded", function(event) {
     Setup.load();
 
+    Setup.registerBlockOptionHandler();
     Setup.registerRadioHandler();
     Setup.registerDiyCssHandler();
 });
